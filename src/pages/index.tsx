f632204a4779565a2d642326ac937b98d51a2661
MeganeMidori <MeganeMidori@users.noreply.github.com>
2021-05-12 19:24:29 -0700

    with socket.io

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { io } from "socket.io-client";
 
 export default function Home() {
+  useEffect(() => {
+    const socket = io();
+
+    socket.on("ping", (message) => {
+      console.log("ping", message)
+      socket.emit("pong", Date.now());
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,14 +24,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to
-          {' '}
-          <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing
-          {' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -62,8 +69,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by
-          {' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,39 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+import { Server } from "socket.io";
 
-const port = parseInt(process.env.PORT || '3002', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT || "3002", 10);
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+const main = async () => {
+  await app.prepare();
 
+  const server = createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true);
+    handle(req, res, parsedUrl);
+  });
+
+  const io = new Server(server);
+
+  io.on("connection", (socket) => {
+    console.log("connection");
+    socket.emit("ping", Date.now());
+
+    socket.on("pong", (message) => {
+      console.log("pong", message);
+    });
+  });
+
+  server.listen(port);
   // tslint:disable-next-line:no-console
   console.log(
     `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
+      dev ? "development" : process.env.NODE_ENV
     }`
-  )
-})
+  );
+};
+
+main();
