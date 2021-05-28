ee3d133ecffedb208975ee6577c0ff6ef9460a5f
MeganeMidori <MeganeMidori@users.noreply.github.com>
2021-05-27 17:09:47 -0700

    remove guest button

--- src/components/player/newGame.tsx
+++ src/components/player/newGame.tsx
@@ -11,11 +11,11 @@ const NewGameComponent: ComponentType<Props> = ({ login }) => (
       <div>
         <a href="/api/auth" className="btn btn-paper">Join With Twitch!</a>
       </div>
-      <div>
+      {/* <div>
         <button type="button" onClick={login} className="btn btn-paper">
           Join as guest
         </button>
-      </div>
+      </div> */}
     </div>
     <div className="alligator-containment-zone">
       <img className="alligator-waiting" src="/loading-alligator.gif" />
